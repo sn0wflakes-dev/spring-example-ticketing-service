f212ed79e3e7f5f36c0e5c41e519285910baeb0c
Abdurrahman ali <abdurrahmanali.dev@gmail.com>
2026-01-21 00:59:38 +0700

    init: initialize spring project

---
+++ src/main/java/aji/learning/ticketing_service/TicketingServiceApplication.java
@@ -0,0 +1,13 @@
+package aji.learning.ticketing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingServiceApplication.class, args);
+    }
+
+}
